50433363347579a3635354bdd96e96c3c1d5259f
Akos Nemes <v-anemes@expediagroup.com>
2021-06-03 13:32:09 +0200

    enhance logging

--- src/main/java/com/akos/threadpool/ThreadPoolParallelStreamTest.java
+++ src/main/java/com/akos/threadpool/ThreadPoolParallelStreamTest.java
@@ -8,36 +8,49 @@ import com.google.common.util.concurrent.*;
 
 public class ThreadPoolParallelStreamTest {
 
+    public static final int END_INCLUSIVE = 1000;
+
     public static void main(String[] args) throws Exception {
         new ThreadPoolParallelStreamTest().run();
     }
 
     private void run() throws ExecutionException, InterruptedException {
         ExecutorService executor = createThreadPool();
+        long start = currentTime();
         System.out.println("Stating in main");
         List<Long> futures = runStream();
+        System.out.printf("Running in main thread took: %d \r\n", currentTime()-start );
 
+        start = currentTime();
         System.out.println("Stating in executor");
         List<CompletableFuture<Long>> futures2 = executor.submit(() ->
                 getFutures(executor))
                 .get();
 
-        CompletableFuture.allOf(futures2.toArray(CompletableFuture[]::new)).join();
+        CompletableFuture.allOf(futures2.toArray(CompletableFuture[]::new))
+                .thenApplyAsync(v -> futures2.stream().map(CompletableFuture::join).collect(Collectors.toList())).join();
+        System.out.printf("Running in executor thread took: %d \r\n", currentTime()-start );
 
+        start = currentTime();
         System.out.println("Stating in executor w/o future");
         List<Long> longs = executor.submit(this::runStream)
                 .get();
+        System.out.printf("Running in executor w/o future thread took: %d \r\n", currentTime()-start );
 
     }
 
+    private long currentTime() {
+        return new Date().getTime();
+    }
+
     private List<CompletableFuture<Long>> getFutures(ExecutorService executor) {
-        return LongStream.rangeClosed(1, 10).parallel().boxed()
+        return LongStream.rangeClosed(1, END_INCLUSIVE).parallel().boxed()
                 .map(l -> CompletableFuture.supplyAsync(() -> doSomething(l), executor))
                 .collect(Collectors.toList());
     }
 
     private List<Long> runStream() {
-        return LongStream.rangeClosed(1, 10).parallel().boxed()
+        return LongStream.rangeClosed(1, END_INCLUSIVE).parallel().boxed()
                 .map(this::doSomething)
                 .collect(Collectors.toList());
     }
@@ -63,7 +76,7 @@ public class ThreadPoolParallelStreamTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.printf("Thread name: %s \r\n", Thread.currentThread().getName());
+        System.out.printf("[%d]Thread name: %s \r\n", l, Thread.currentThread().getName());
         return l;
     }
 }
